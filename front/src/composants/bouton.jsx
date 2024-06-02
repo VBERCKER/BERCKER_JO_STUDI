01e484abdae8388327d1d888c77dc91046b39146
VBERCKER <138994243+VBERCKER@users.noreply.github.com>
2024-06-02 10:36:00 +0200

    vercel

--- front/src/composants/bouton.jsx
+++ front/src/composants/bouton.jsx
@@ -2,12 +2,9 @@ import React from "react";
 
 function Boutton(props){
     return(
-        <a href={props.lien}>
-
-            <button className="button1" value={props.value} onClick={props.click}>
+        <button className="button1" value={props.value} onClick={props.click}>
        {props.btn}
         </button>
-        </a> 
 
     )
 }
--- front/src/composants/card.jsx
+++ front/src/composants/card.jsx
@@ -14,29 +14,39 @@ function Carte (props){
     <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
       <div className="col d-flex align-items-start">
         <div className="icon-square text-body-emphasis bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
-         
+          <svg className="bi" width="1em" height="1em"><use></use></svg>
         </div>
         <div>
           <h3 className=" police-titre fs-2">{props.hsport}</h3>
           <p className="police-p">{props.psport}</p>
-          
+          <a className="service-a"  href="#">
+            <Button btn={"bouton"}/>
+          </a>
         </div>
       </div>
       <div className="col d-flex align-items-start">
-      
+        <div className="icon-square text-body-emphasis bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+          <svg className="bi" width="1em" height="1em"><use></use></svg>
+        </div>
         <div className="te">
           <h3 className=" police-titre fs-2 text-body-emphasis">{props.hoffre}</h3>
           <p className="police-p">{props.poffre}</p>
           
-        
+          <a  href="#" >
+           <Button btn={"bouton"}/>
+          </a>
         </div>
       </div>
       <div className="col d-flex align-items-start">
-       
+        <div className="icon-square text-body-emphasis bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+          <svg className="bi" width="1em" height="1em"><use ></use></svg>
+        </div>
         <div>
           <h3 className=" police-titre fs-2 text-body-emphasis">{props.hjeu}</h3>
           <p className="police-p">{props.pjeu}</p>
-          
+          <a className="police-p" href="#" >
+            <Button btn={"bouton"}/>
+          </a>
         </div>
       </div>
     </div>
